d1767e0aea94c3b24efcd2fd190c0676795580a3
tkuwill <86701769+tkuwill@users.noreply.github.com>
2021-08-08 15:18:55 +0800

    Add files via upload

---
+++ letcomputersleep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author:Will  email:billwang1023@gmail.com
+# Date:2021/07/26 
+# Place:Taiwan
+# Description: let computer sleep after ? min(s).
+# Thank you for using this shell script, in order to make this run, change the access permission of the shell script with chmod +x .Then you can run theshell script by "./letcomputersleep.sh".
+echo "Input mins:"
+read mins
+echo "Now time is" 
+date
+echo "your computer will sleep after ${mins} minute(s)."
+
+sleep ${mins}m && systemctl suspend -i
