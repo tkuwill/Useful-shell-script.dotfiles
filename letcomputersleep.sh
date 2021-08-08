a5db748cc6cdd9d941c30742d5099055e4f60ccc
tkuwill <86701769+tkuwill@users.noreply.github.com>
2021-08-08 15:48:55 +0800

    Update letcomputersleep.sh

--- letcomputersleep.sh
+++ letcomputersleep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author:Will  email:billwang1023@gmail.com
-# Date:2021/08/08 
+# Version:2021/08/08 v1.0 
 # Place:Taiwan
 # Description: let computer sleep after ? min(s).
 # Thank you for using this shell script, in order to make this run, change the access permission of the shell script with chmod +x .Then you can run theshell script by "./letcomputersleep.sh".
