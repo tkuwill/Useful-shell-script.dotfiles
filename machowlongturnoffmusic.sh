c06d09091dc6529ab30dd25d849e1a78b8d323bc
tkuwill <86701769+tkuwill@users.noreply.github.com>
2021-08-23 07:15:09 +0800

    Update machowlongturnoffmusic.sh

--- machowlongturnoffmusic.sh
+++ machowlongturnoffmusic.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-# Author:Will  email:billwang1023@gmail.com
+# Author:Will  email:billwang1023+github@gmail.com
 # Date:2021/07/26 Version1.0
 # Date:2021/08/23 Version2.0 
 # Place:Taiwan
 # Description: close Music after ? sec(s)
-# Thank you for using this shell script, in order to make this run, change the access permission of the shell script with chmod +x .Then you can run theshell script by "./howlongturnoffmusic.sh".
+# Thank you for using this shell script, in order to make this run, change the access permission of the shell script with chmod +x .Then you can run the shell script by "./machowlongturnoffmusic.sh".
 echo "Input secs:"
 read secs
 echo "Now time is" && date
