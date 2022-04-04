d86132a37ef23dcc3b4d27204eeeb8452b646186
tkuwill <86701769+tkuwill@users.noreply.github.com>
2022-04-05 00:15:37 +0800

    Update reminder.sh

--- reminder.sh
+++ reminder.sh
@@ -11,4 +11,4 @@ echo "Now time is "
 date
 echo "Set a timer for ${mins} minute(s)."
 sleep ${mins}m && notify-send -i Pictures/DesktopBackground/arch-removebg-preview.png -u critical -t 5000 ${msg}
-
+mpv ~/Music/lovesongs/+\ 1day.mp3 --start=0 --end=10 --keep-open=no --no-resume-playback --no-terminal
